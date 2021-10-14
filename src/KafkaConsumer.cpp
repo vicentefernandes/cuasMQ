78830acbf21b13affee9bd447735fd21e6fef43d
vmachado <vmachado@gradiant.org>
2021-10-14 21:50:31 +0100

    mess 01

---
+++ src/KafkaConsumer.cpp
@@ -0,0 +1,4 @@
+//
+// Created by vicente on 10/13/21.
+//
+
---
+++ src/cuas_consumer_example.cpp
@@ -0,0 +1,56 @@
+//
+// Created by vicente on 10/13/21.
+//
+
+#include "KafkaConsumer.h"
+
+
+class Mano {
+public:
+    Mano(std::string s, int i) :  x(i) , y(s){}
+    int x;
+    std::string y;
+};
+
+
+void cb01(const std::string data, std::any x) {
+    std::cout << "data:  >>> " << data << std::endl;
+
+    auto x_ = std::any_cast<std::shared_ptr<Mano>>(x);
+    std::cout << "data: mano >>> " << ++x_->x <<  " " << x_->y << std::endl;
+}
+
+void cb02(const std::string data, std::any x) {
+    std::cout << "CB0202020202020200202 :  >>>> " << data << std::endl;
+
+    auto x_ = std::any_cast<std::vector<Mano>>(x);
+    std::cout << "data: x >>>> " << x_[0].x <<  " " << x_[1].y << std::endl;
+}
+
+
+
+
+int main(){
+    KafkaConsumer kc("localhost:9092", 1);
+    Mano mano2("fdx", 777);
+
+    //if you want to use your current variables data inside the callback you must store it as shared_ptr
+    //and pass the shared_prt as subscribe() arg
+    //otherwise, the data will be passed as copy
+
+    auto mano = std::make_shared<Mano>("carallo", 333);
+    kc.subscribe("topic13", cb01, mano);
+
+    std::vector<Mano> mvec = {mano2, mano2};
+    kc.subscribe("topic31", cb02, mvec);
+
+    ///TODO subscrive more topics and impleempt their _callbacks
+    /// it requires getting the topic name inside proccess func (currently the topic is hardcoded)
+    kc.process();
+
+
+    while(true) {
+        sleep(4);
+        std::cout << "main mano: " << mano->x << " " << mano->y << std::endl;
+    }
+}
